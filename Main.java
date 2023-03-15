d9e8badf8207549cd71d4e07bb649fbbc5634e8f
JDuer02 <126687677+JDuer02@users.noreply.github.com>
2023-03-15 14:02:13 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Wprowadź 3 dowolne liczby");
+    Scanner scanner = new Scanner(System.in);
+    double a = scanner.nextDouble();
+    double b = scanner.nextDouble();
+    double c = scanner.nextDouble();
+
+    double max=0;
+    if (a > b && a>c)
+      max=a;
+    else if (a < c && b < c)
+      max=c;
+    else if (a < b && b > c)
+      max =b;
+
+    System.out.println(max);
+  }
+}
